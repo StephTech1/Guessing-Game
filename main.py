0838a720b9fcbe35dca19696d220a5b71d8841c6
Steph St <steph.streete@outlook.com>
2020-10-01 23:19:33 +0000

    added more code for chances

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-#Set a variable to be a random number between 1 and 10. Ask user to guess the number. Print an output if guessed correctly.  
-# Extend the guessing game. Give the user 6 chances to guess the number using a while loop. Let them know if it is too high or too low
-
 print("Guessing Game")
 guess = 0
 
@@ -8,12 +5,15 @@ number = int(input("Please choose a number between 1 and 10:"))
 
 if (number == 6):
   print("You guessed correctly!")
+if (number != 6):
+  print("You have 5 more chances")
+
+number = int(input("Please choose a number between 1 and 10:"))
 
 while number >6 :
   guess = guess +1
   print("You guessed too high!")
   break
-
 while number <6 :
   guess = guess -1
   print("You guessed too low!")
