484b43c4eafd8c450539e6f4c2498d75d66b0593
Steph St <steph.streete@outlook.com>
2020-10-01 22:57:21 +0000

    Added break to loop

--- main.py
+++ main.py
@@ -2,14 +2,19 @@
 # Extend the guessing game. Give the user 6 chances to guess the number using a while loop. Let them know if it is too high or too low
 
 print("Guessing Game")
+guess = 0
 
 number = int(input("Please choose a number between 1 and 10:"))
 
 if (number == 6):
   print("You guessed correctly!")
 
-
 while number >6 :
+  guess = guess +1
   print("You guessed too high!")
-else number <6 :
+  break
+
+while number <6 :
+  guess = guess -1
   print("You guessed too low!")
+  break
