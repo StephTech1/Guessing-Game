1f4f10c5ad7672b2b3ee2720964b477e79c3a88e
Steph St <steph.streete@outlook.com>
2020-10-01 19:18:18 +0000

    Entered question for now

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
-#Set a variable to be a random number between 1 and 10. Ask user to guess the number. PRint an output if guessed correctly.  
+#Set a variable to be a random number between 1 and 10. Ask user to guess the number. Ppint an output if guessed correctly.  
 # Extend the guessing game. Give the user 6 chances to guess the number using a while loop. Let them know if it is too high or too low
