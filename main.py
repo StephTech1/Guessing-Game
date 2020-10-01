30308052503408f33be4e8d18ecd56381c871943
Steph St <steph.streete@outlook.com>
2020-10-01 22:14:32 +0000

    Entered more to the code

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 # Extend the guessing game. Give the user 6 chances to guess the number using a while loop. Let them know if it is too high or too low
 
 print("Guessing Game")
-print("Please choose a random number between 1 and 10")
 
-random_number = randit(1,9)
+number = int(input("Please choose a number between 1 and 10:"))
+
+while int !=6:
+  print("You guessed correctly!")
