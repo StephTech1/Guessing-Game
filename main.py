dbb9651b37e4a7ddd48fe3a10ae5aa75f1570294
Steph St <steph.streete@outlook.com>
2020-10-02 00:06:15 +0000

    Moved indentations into right place

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
-print("Guessing Game")
+print("Guessing The Number Game")
 guess = 0
 
 number = int(input("Please choose a number between 1 and 10:"))
-
 if (number == 6):
   print("You guessed correctly!")
 if (number != 6):
-  print("You have 5 more chances")
+  print("You have 5 more chances.....")
+while number >6:
+  guess = guess +1
+  print("You guessed too high!")
+  break
+while number <6:
+  guess = guess -1
+  print("You guessed too low!")
+  break
 
 number = int(input("Please choose a number between 1 and 10:"))
-
+if (number == 6):
+  print("You guessed correctly!")
+if (number != 6):
+  print("You have 4 more chances....")
 while number >6 :
   guess = guess +1
   print("You guessed too high!")
@@ -17,4 +27,56 @@ while number >6 :
 while number <6 :
   guess = guess -1
   print("You guessed too low!")
+  break
+
+number = int(input("Please choose a number between 1 and 10:"))
+if (number == 6):
+  print("You guessed correctly!")
+if (number != 6):
+  print("You have 3 more chances...")
+while number >6 :
+  guess = guess +1
+  print("You guessed too high!")
+  break
+while number <6 :
+  guess = guess -1
+  print("You guessed too low!")
+  break
+  
+number = int(input("Please choose a number between 1 and 10:"))
+if (number == 6):
+  print("You guessed correctly!")
+if (number != 6):
+  print("You have 2 more chances..")
+while number >6 :
+  guess = guess +1
+  print("You guessed too high!")
+  break
+while number <6 :
+  guess = guess -1
+  print("You guessed too low!")
+  break
+
+number = int(input("Please choose a number between 1 and 10:"))
+if (number ==  6):
+  print("You have 1 more chance.")
+while number >6 :
+  guess = guess -1
+  print("You guessed too high!")
+  break
+while number <6 :
+    print("You guessed too low")
+    break
+
+number = int(input("Please choose a number between 1 and 10"))
+if (number == 6):
+  print("You guessed correctly!")
+if (number != 6):
+  print("This is your last chance!!")
+while number >6:
+  guess = guess +1
+  print("You guessed too high!")
+  break
+while number <6 :
+  print("You guessed too low")
   break
